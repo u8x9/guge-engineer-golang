8165b242ea532c5cf6f5195ec9dea4ffafcffeb7
u8x9 <u8x@eclipso.eu>
2020-11-30 02:16:06 +0800

    条件判断

--- ch02/04branch/main.go
+++ ch02/04branch/main.go
@@ -5,6 +5,24 @@ import (
 	"io/ioutil"
 )
 
+func grade(score int) (s string) {
+	switch {
+	case score <60:
+		s="F"
+	case score <70:
+		s="D"
+	case score<80:
+		s="C"
+	case score <90:
+		s="B"
+	case score<=100:
+		s="A"
+	case score<0||score>10:
+		panic("非法数据")
+	}
+
+	return
+}
 func main() {
 	const filename = "main.go"
 	contents, err := ioutil.ReadFile(filename)
@@ -13,4 +31,7 @@ func main() {
 		return
 	}
 	fmt.Printf("%s\n", contents)
+
+	s:=grade(91)
+	fmt.Println(s)
 }
